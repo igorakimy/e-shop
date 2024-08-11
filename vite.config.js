157d24ebe993d2a3575d9bb149f13ba9c05e64d0
aki <igorakimy@gmail.com>
2024-08-11 17:33:17 +0300

    add: install + config inertia and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            'ziggy-js': path.resolve('/vendor/tightenco/ziggy')
+        }
+    },
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    }
 });
